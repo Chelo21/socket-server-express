9f1cd31751afea337dfb2fe351fa7aa9b4534f44
Chelo21 <ortamar@gmail.com>
2021-08-20 01:27:26 -0300

    express 2

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const socketio = require('socket.io')
 const path = require('path')
 const Sockets = require('./sockets')
+const cors = require('cors')
 
 class Server {
 
@@ -23,6 +24,7 @@ this.io = socketio(this.server, {/*   configuraciones */ });
 middlwares(){
 //desplegar el directorio publico
 this.app.use(express.static(path.resolve(__dirname,'../public' )           ))
+this.app.use(cors())
 
 }
 
